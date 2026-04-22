2215071c1f2876eff198a8ae450fa62556610988
yoshiio10 <L22070340@cdmadero.tecnm.mx>
2026-04-22 15:34:20 -0600

    fix ssl supabase

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false }
 });
 
 const authRoutes = require('./routes/auth');
