064f7becc478b1391c3a5e30c2d21d2ae7c4c2a1
yoshiio10 <L22070340@cdmadero.tecnm.mx>
2026-04-22 15:27:52 -0600

    fix cors

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'authorization']
+}));
 
 const pool = new Pool({
   host: process.env.DB_HOST,
